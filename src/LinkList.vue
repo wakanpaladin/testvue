05d6622c397598d474f6f69089d621fa7c27e9ee
yuan.wang <yuan.wang@sky-data.cn>
2017-06-22 21:28:56 +0800

    first

---
+++ src/LinkList.vue
@@ -0,0 +1,21 @@
+<template>
+
+<ul>
+  <li v-for="l in links" target>
+  	<a :href=l.link target="_blank">{{l.name}}</a>
+  </li>
+</ul>
+</template>
+
+<script type="text/javascript">
+export default {
+	data(){
+		return {
+			links:[
+				{link:'https://vuejs.org',name:'vuejs'},
+				{link:'https://vuejs.org',name:'vuejs2'},
+			]
+		}
+	}
+}
+</script>
